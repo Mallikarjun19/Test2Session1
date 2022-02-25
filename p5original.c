e1dea7165652aa577f7c4746cd645d5edeb59d83
Mallikarjun19 <96414039+Mallikarjun19@users.noreply.github.com>
2022-02-25 05:00:03 +0000

    g

---
+++ p5original.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("enter the array size\n");
+  scanf("%d",n);
+  return ;
+}
+void init_array(int n,int a[n])
+{
+  for(i=0;i<n;i++)
+    {
+      a[i]=i;
+    }
+a[1]=0; 
+}
+void erotosthenes_sieve(int n,int a[n])
+{
+  int i=0;
+  while()
+}
